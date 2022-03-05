eac87752a18af5e49557de002de8236651b451a6
Mosabbir Ahamed <90866410+MosabbirAhamed@users.noreply.github.com>
2022-03-05 20:16:10 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Toggle Mobile Menu
+const menuBtn = document.querySelector('.nav_bar')
+const nav = document.querySelector('.mainMenuRight')
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav_active')
+    
+    if(menuBtn.classList.contains('fa-bars')) {
+        menuBtn.classList.remove('fa-bars')
+        menuBtn.classList.add('fa-xmark')
+    }else{
+        menuBtn.classList.add('fa-bars')
+        menuBtn.classList.remove('fa-xmark')
+    }
+})
